5ef685b71676d823971e3fed15bca2c566e99aa6
mtrtrdev <mtrtrdev@gmail.com>
2025-07-05 11:21:57 +0900

    first commit

--- app/main.py
+++ app/main.py
@@ -1,6 +1,12 @@
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Hello from Python in Docker!"
+@app.route('/')
+def hello_world():
+    return 'Hello, Docker!'
+
+if __name__ == '__main__':
+    # host='0.0.0.0' を指定することが重要
+    # portはDockerのEXPOSEやrunコマンドの-pと合わせる
+    app.run(host='0.0.0.0', port=5000)
